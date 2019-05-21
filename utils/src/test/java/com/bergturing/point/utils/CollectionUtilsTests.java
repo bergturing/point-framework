5d23d6e882e21c2cf7426ff922c3272b686e2591
berg-turing <993450432@qq.com>
2019-05-21 11:30:26 +0800

    utils：新增常用的工具类

---
+++ utils/src/test/java/com/bergturing/point/utils/CollectionUtilsTests.java
@@ -0,0 +1,88 @@
+package com.bergturing.point.utils;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.LongStream;
+
+import static com.bergturing.point.utils.CollectionUtils.*;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * 集合工具类的测试类
+ *
+ * @author bergturing@qq.com
+ * @apiNote 2019/5/19
+ */
+public class CollectionUtilsTests {
+    @Test
+    public void testIsEqual() {
+        List<Long> collectionT;
+        List<Long> collectionU;
+
+        // 测试匹配
+        for (int index = 10; index < 1000; index++) {
+            collectionT = LongStream.rangeClosed(1, index).boxed().collect(toList());
+            collectionU = LongStream.rangeClosed(1, index).boxed().collect(toList());
+            assert isEqual(collectionT, collectionU, Long::equals);
+        }
+
+        // 测试不匹配
+        for (int index = 10; index < 1000; index++) {
+            collectionT = LongStream.rangeClosed(1, index).boxed().collect(toList());
+            collectionU = LongStream.rangeClosed(1, index + 1).boxed().collect(toList());
+            assert !isEqual(collectionT, collectionU, Long::equals);
+        }
+    }
+
+    @Test
+    public void testDistinctEqual() {
+        List<Long> collectionT;
+        List<Long> collectionU;
+
+        // 测试匹配
+        for (int index = 10; index < 1000; index++) {
+            collectionT = LongStream.rangeClosed(1, index).boxed().collect(toList());
+            collectionT.addAll(LongStream.rangeClosed(1, index).boxed().collect(toList()));
+
+            collectionU = LongStream.rangeClosed(1, index).boxed().collect(toList());
+            assert distinctEqual(collectionT, collectionU, Long::equals);
+        }
+
+        // 测试不匹配
+        for (int index = 10; index < 1000; index++) {
+            collectionT = LongStream.rangeClosed(1, index).boxed().collect(toList());
+            collectionT.addAll(LongStream.rangeClosed(1, index).boxed().collect(toList()));
+
+            collectionU = LongStream.rangeClosed(1, index + 1).boxed().collect(toList());
+            assert !distinctEqual(collectionT, collectionU, Long::equals);
+        }
+    }
+
+    @Test
+    public void testInclude() {
+        List<Long> collectionT;
+        List<Long> collectionU;
+
+        // 测试包含
+        for (int index = 10; index < 1000; index++) {
+            collectionT = LongStream.rangeClosed(1, index).boxed().collect(toList());
+            collectionU = LongStream.rangeClosed(1, index / 2).boxed().collect(toList());
+            assert include(collectionT, collectionU, Long::equals);
+        }
+
+        // 测试不包含
+        for (int index = 10; index < 1000; index++) {
+            collectionT = LongStream.rangeClosed(1, index).boxed().collect(toList());
+            collectionU = LongStream.rangeClosed(1, index + 1).boxed().collect(toList());
+            assert !include(collectionT, collectionU, Long::equals);
+        }
+
+        // 测试不包含
+        for (int index = 10; index < 1000; index++) {
+            collectionT = LongStream.rangeClosed(1, index).boxed().collect(toList());
+            collectionU = LongStream.rangeClosed(-1, index / 2).boxed().collect(toList());
+            assert !include(collectionT, collectionU, Long::equals);
+        }
+    }
+}
---
+++ utils/src/test/java/com/bergturing/point/utils/entity/Entity.java
@@ -0,0 +1,53 @@
+package com.bergturing.point.utils.entity;
+
+/**
+ * 实体工具类的辅助测试对象
+ *
+ * @author bergturing@qq.com
+ * @apiNote 2019/5/19
+ */
+public class Entity {
+    /**
+     * id   该字段用于测试通过方法设置值
+     */
+    private Long id;
+
+    /**
+     * 名称   该字段用于测试通过字段设置值
+     */
+    private String name;
+
+    /**
+     * 年龄   该字段用于测试通过方法获取值
+     */
+    private Long age;
+
+    /**
+     * 备注   该字段用于测试通过字段获取值
+     */
+    private String remarks;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+}
